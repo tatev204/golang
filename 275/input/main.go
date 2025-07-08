e08ab41f447d5907164a7ec0ec973df433d8ca83
Tatev <tatevik502@gmail.com>
2025-07-08 17:38:54 +0400

    275

---
+++ 275/input/main.go
@@ -0,0 +1,24 @@
+package input
+
+import (
+	"fmt"
+)
+
+func CountNumberElementValueLessK(arr []rune, k rune) int {
+	count := 0
+	for _, val := range arr {
+		if val < k {
+			count++
+		}
+	}
+	return count
+}
+
+func InputArray(name string, n int) []rune {
+	arr := make([]rune, n)
+	fmt.Printf("Enter %d letters for %s:\n", n, name)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%c", &arr[i])
+	}
+	return arr
+}
---
+++ 275/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"project6/input"
+)
+
+func main() {
+	var n int
+
+	fmt.Print("enter number of letters ")
+	fmt.Scan(&n)
+
+	arr := input.InputArray("Array", n)
+
+	var k rune = 'k'
+	fmt.Println("enter letter k:")
+	fmt.Printf("%c\n", k)
+
+	count := input.CountNumberElementValueLessK(arr, k)
+	fmt.Printf("'%c' has numeric value %d, and %d letters are less than it\n", k, k, count)
+
+}
